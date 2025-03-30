14c06c693b0a01ba42ccaca098116be8a01579c0
Aleksi Kangas <aleksi.kangas@hotmail.com>
2025-03-30 11:09:13 +0300

    QCT-convert application with command line options

--- main.cpp
+++ main.cpp
@@ -1,22 +1,44 @@
 #include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <optional>
+#include <random>  // TODO REMOVE
+
+#include "CLI11.hpp"
 
 import qct;
+import qctexport;
 
-int main(const int argc, char* argv[]) {
-  if (argc < 2) {
-    std::cerr << "Usage: " << argv[0] << " <path-to-qct-file>" << " " << "<path-to-kml-export-file>" << std::endl;
-    return 1;
-  }
-  const std::filesystem::path qct_file_path{argv[1]};
-  const std::filesystem::path kml_export_file_path{argv[2]};
+int main(const int argc, char** argv) {
+  CLI::App app{"QCT Convert"};
+  argv = app.ensure_utf8(argv);
+
+  std::filesystem::path qct_file_path{};
+  std::filesystem::path bmp_export_file_path{};
+  std::filesystem::path kml_export_file_path{};
+  std::filesystem::path png_export_file_path{};
+  app.add_option("-p,--path", qct_file_path, "Path to the .qct file");
+  app.add_option("--export-bmp-path", bmp_export_file_path, "Path to optional .bmp export");
+  app.add_option("--export-kml-path", kml_export_file_path, "Path to optional .kml export");
+  app.add_option("--export-png-path", png_export_file_path, "Path to optional .png export");
+  CLI11_PARSE(app, argc, argv);
+
   if (exists(qct_file_path)) {
     if (is_regular_file(qct_file_path)) {
       std::ifstream file{qct_file_path, std::ios::binary};
-      std::cout << "Reading " << qct_file_path.string() << std::endl;
-      qct::QctFile qct_file = qct::QctFile::parse(file, std::make_optional(kml_export_file_path));
+      try {
+        const qct::QctFile qct_file = qct::QctFile::parse(file);
+        if (!bmp_export_file_path.empty()) {
+          qct::ex::bmp::exportBmp(bmp_export_file_path, qct_file);
+        }
+        if (!kml_export_file_path.empty()) {
+          qct::ex::kml::exportKml(kml_export_file_path, qct_file);
+        }
+        if (!png_export_file_path.empty()) {
+          qct::ex::png::exportPng(png_export_file_path, qct_file);
+        }
+      } catch (const qct::common::QctException& e) {
+        std::cerr << e.what() << std::endl;
+      }
     } else {
       std::cerr << "Not a file." << std::endl;
     }
