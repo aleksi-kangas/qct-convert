e491e692997519b53f49a5d55aad9b7419ad9555
Aleksi Kangas <aleksi.kangas@hotmail.com>
2025-10-26 14:22:20 +0200

    Palette parsing

---
+++ qct-core/src/main/java/com/github/aleksikangas/qct/core/color/Palette.java
@@ -0,0 +1,71 @@
+package com.github.aleksikangas.qct.core.color;
+
+import com.github.aleksikangas.qct.core.parser.AbstractParser;
+import com.github.aleksikangas.qct.core.parser.Parseable;
+import com.github.aleksikangas.qct.core.parser.ParserRegistry;
+import com.github.aleksikangas.qct.core.reader.QctReader;
+
+import java.awt.*;
+import java.nio.channels.AsynchronousFileChannel;
+
+/**
+ * <pre>
+ * +--------+--------------+------------------------------+
+ * | Offset | Size (Bytes) | Content                      |
+ * +--------+--------------+------------------------------+
+ * | 0x01A0 | 256 x 4      | Palette - 128 of 256 Colours |
+ * +--------+--------------+------------------------------+
+ *
+ * +--------+-----------+-------------------------+
+ * | Offset | Data Type | Content                 |
+ * +--------+-----------+-------------------------+
+ * | 0x00   | Byte      | Blue Intensity [0-255]  |
+ * | 0x01   | Byte      | Green Intensity [0-255] |
+ * | 0x02   | Byte      | Red Intensity [0-255]   |
+ * | 0x03   | Byte      | Padding byte, set to 0  |
+ * +--------+-----------+-------------------------+
+ * </pre>
+ */
+public record Palette(Color[] colors) implements Parseable {
+  public static final long BYTE_OFFSET = 0x01A0L;
+  public static final int SIZE = 128;
+
+  @Override
+  public String toString() {
+    final StringBuilder stringBuilder = new StringBuilder();
+    for (int i = 0; i < SIZE; ++i) {
+      final Color color = colors[i];
+      stringBuilder.append(String.format("\t%3d: [R: %3d, G: %3d, B: %3d]",
+                                         i,
+                                         color.getRed(),
+                                         color.getGreen(),
+                                         color.getBlue()));
+      if (i < SIZE - 1) {
+        stringBuilder.append("\n");
+      }
+    }
+    return stringBuilder.toString();
+  }
+
+  public static final class Parser extends AbstractParser<Palette> {
+    @Override
+    public Class<Palette> parseableClass() {
+      return Palette.class;
+    }
+
+    @Override
+    public Palette parse(final AsynchronousFileChannel asyncFileChannel,
+                         final long byteOffset,
+                         final ParserRegistry parserRegistry) {
+      final int[] bytes = QctReader.readBytes(asyncFileChannel, byteOffset, SIZE * 4);
+      final Color[] colors = new Color[SIZE];
+      for (int i = 0; i < SIZE; ++i) {
+        final int blue = bytes[i * 4];
+        final int green = bytes[i * 4 + 1];
+        final int red = bytes[i * 4 + 2];
+        colors[i] = new Color(red, green, blue);
+      }
+      return new Palette(colors);
+    }
+  }
+}
