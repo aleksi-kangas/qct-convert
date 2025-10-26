e0a9a9fb5f7d6494de9b14fa4c2a9b1f35072122
Aleksi Kangas <aleksi.kangas@hotmail.com>
2025-10-26 14:22:31 +0200

    Interpolation Matrix parsing

---
+++ qct-core/src/main/java/com/github/aleksikangas/qct/core/color/InterpolationMatrix.java
@@ -0,0 +1,58 @@
+package com.github.aleksikangas.qct.core.color;
+
+import com.github.aleksikangas.qct.core.parser.AbstractParser;
+import com.github.aleksikangas.qct.core.parser.Parseable;
+import com.github.aleksikangas.qct.core.parser.ParserRegistry;
+import com.github.aleksikangas.qct.core.reader.QctReader;
+
+import java.nio.channels.AsynchronousFileChannel;
+
+/**
+ * <pre>
+ * +--------+---------------+---------------------+
+ * | Offset | Size (Bytes) | Content              |
+ * +--------+--------------+----------------------+
+ * | 0x05A0 | 128 x 128    | Interpolation Matrix |
+ * +--------+--------------+----------------------+
+ *
+ * +--------+-----------+---------------------------+
+ * | Offset | Data Type | Content                   |
+ * +--------+-----------+---------------------------+
+ * | 0x0000 | Byte      | Precalculated Color Index |
+ * | 0x0001 | Byte      | Precalculated Color Index |
+ * | ...    | ...       | ...                       |
+ * | 0x4000 | Byte      | Precalculated Color Index |
+ * +--------+-----------+---------------------------+
+ * </pre>
+ * The offset of any given row/column index into the matrix is given by: {@code offset = (128 x y) + x}.
+ * Due to symmetry, {@code y} and {@code x} are interchangeable.
+ */
+public record InterpolationMatrix(int[] indices) implements Parseable {
+  public static final long BYTE_OFFSET = 0x05A0L;
+  public static final int SIZE_COLUMNS = 128;
+  public static final int SIZE_ROWS = 1288;
+  public static final int SIZE = SIZE_COLUMNS * SIZE_ROWS;
+
+  public int colorIndexOf(final int yColorIndex, final int xColorIndex) {
+    return indices[offsetOf(yColorIndex, xColorIndex)];
+  }
+
+  public static int offsetOf(final int y, final int x) {
+    return (128 * y) + x;
+  }
+
+  public static final class Parser extends AbstractParser<InterpolationMatrix> {
+
+    @Override
+    public Class<InterpolationMatrix> parseableClass() {
+      return InterpolationMatrix.class;
+    }
+
+    @Override
+    public InterpolationMatrix parse(final AsynchronousFileChannel asyncFileChannel,
+                                     final long byteOffset,
+                                     final ParserRegistry parserRegistry) {
+      return new InterpolationMatrix(QctReader.readBytes(asyncFileChannel, byteOffset, SIZE));
+    }
+  }
+}
---
+++ qct-core/src/test/java/com/github/aleksikangas/qct/core/color/InterpolationMatrixTest.java
@@ -0,0 +1,33 @@
+package com.github.aleksikangas.qct.core.color;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+/**
+ * Tests for {@link InterpolationMatrix}.
+ */
+public final class InterpolationMatrixTest {
+  @Test
+  public void test() {
+    final var interpolationMatrix = generateSymmetric();
+    assertEquals(2, interpolationMatrix.colorIndexOf(2, 0));
+    assertEquals(2, interpolationMatrix.colorIndexOf(0, 2));
+    assertEquals(258, interpolationMatrix.colorIndexOf(2, 2));
+  }
+
+  private static InterpolationMatrix generateSymmetric() {
+    final int[] m = new int[InterpolationMatrix.SIZE];
+    IntStream.range(0, InterpolationMatrix.SIZE_ROWS)
+             .forEach(y -> IntStream.range(0, InterpolationMatrix.SIZE_COLUMNS)
+                                    .forEach(x -> {
+                                      final int minIdx = Math.min(y, x);
+                                      final int maxIdx = Math.max(y, x);
+                                      final int value = (minIdx * InterpolationMatrix.SIZE_COLUMNS) + maxIdx;
+                                      m[(y * InterpolationMatrix.SIZE_COLUMNS) + x] = value;
+                                    }));
+    return new InterpolationMatrix(m);
+  }
+}
