bc5497194a9bd725c3cf11be9ee8a09e341da17d
Aleksi Kangas <aleksi.kangas@hotmail.com>
2025-12-15 21:55:03 +0200

    Invoke on EDT

--- qct-ui/src/main/java/com/github/aleksikangas/qct/ui/common/AbstractController.java
+++ qct-ui/src/main/java/com/github/aleksikangas/qct/ui/common/AbstractController.java
@@ -4,17 +4,19 @@ import com.github.aleksikangas.qct.ui.events.decode.DecodeFailureEvent;
 import com.github.aleksikangas.qct.ui.events.decode.DecodeSuccessEvent;
 import jakarta.enterprise.event.ObservesAsync;
 
+import javax.swing.SwingUtilities;
+
 public abstract class AbstractController<T extends AbstractPanel> implements DecodeSuccessEvent.Aware, DecodeFailureEvent.Aware {
   protected T panel;
 
   @Override
   public void onDecodeSuccess(@ObservesAsync final DecodeSuccessEvent event) {
-    panel.onDecodeSuccess(event);
+    SwingUtilities.invokeLater(() -> panel.onDecodeSuccess(event));
   }
 
   @Override
   public void onDecodeFailure(@ObservesAsync final DecodeFailureEvent event) {
-    panel.onDecodeFailure(event);
+    SwingUtilities.invokeLater(() -> panel.onDecodeFailure(event));
   }
 
   public T getPanel() {
